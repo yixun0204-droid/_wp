b88d21bf7a46db159d47a8e7cbbdfa88baba2ee8
Yixun <your_email@example.com>
2026-06-11 05:15:31 +0800

    Deploy finalized build

--- 期中/my-game-project/frontend/next.config.ts
+++ 期中/my-game-project/frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',       // 🎯 告訴 Next.js 把網頁打包成靜態 HTML 檔案
+  basePath: '/_wp',       // 🎯 告訴 Next.js 你的 GitHub 專案倉庫名稱
+  images: {
+    unoptimized: true,    // 靜態導出必須關閉圖片優化
+  },
 };
 
 export default nextConfig;
